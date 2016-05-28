058c7c70c2cced0f2556cebcf9181ef8d5a6a3b3
Beyang Liu <beyang@sourcegraph.com>
2016-05-27 22:33:47 -0700

    stdlib modules

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-stdlib-refs',
+    version='0.0.0',
+    url='http://github.com/sgtest/python-stdlib-refs',
+    packages=['pkg'],
+    install_requires=[],
+)
